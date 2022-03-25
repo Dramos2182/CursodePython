348b52107f4737ff19725f7109584795d43910d2
Dramos2182 <eduardo.ramosm@uanl.edu.mx>
2022-03-25 15:19:43 -0600

    Ejercicios de VUE

---
+++ vuex-launch-x/src/components/botones.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="botones">
+      <button @click="$store.dispatch('bajarcontador')">-</button>
+      <button @click="$store.dispatch('subircontador')">+</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'botones',  
+}
+</script>
--- vuex-launch-x/src/views/HomeView.vue
+++ vuex-launch-x/src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <contador/>
+    <cuadrado/>
+    <botones/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import  contador  from "../components/contador.vue";
+import cuadrado from "../components/cuadrado.vue";
+import botones from "../components/botones.vue"
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components:{
+    contador,
+    cuadrado,
+    botones,
   }
 }
 </script>
+
+<style>
+div{
+  margin-bottom: 10px;
+}
+
+.contador{
+  font-size: 80px;
+}
+.botones button{
+  font-size: 40px;
+  width: 100px;
+  margin: 0px 10px;
+  
+}
+
+.cuadrado{
+  font-size: 25px;
+}
+</style>
